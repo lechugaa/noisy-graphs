8a995685107ad1535092902de558aba112c45b08
Antonio Lechuga <antoniolechugarivera@gmail.com>
2020-09-28 08:57:28 -0500

    Efficient implementations

--- noisy_graph.py
+++ noisy_graph.py
@@ -1,86 +1,248 @@
-import networkx as nx
 import statistics
 from math import log
 from scipy.special import comb
 
 
 class NoisyGraph:
+    """An undirected graph where some of the edges
+    contained are fake.
+    """
+    def __init__(self):
+        """
+        Initializes a noisy graph object.
+        """
+        self.__real_edges = {}
+        self.__fake_edges = {}
 
-    def __init__(self, edges=None):
-        if edges is None:
-            edges = []
-        self.__graph = nx.Graph()
-        self.__graph.add_edges_from(edges)
-        nx.set_edge_attributes(self.__graph, True, name="real")
+    def nodes(self):
+        """
+        Returns all the nodes in the graph
+        :return: list hashable objects
+        """
+        return [node for node in self.__real_edges.keys()]
+
+    def number_of_nodes(self):
+        """Returns the number of nodes in the graph
+        :return: integer
+        """
+        return len(self.nodes())
+
+    def add_node(self, node):
+        """
+        Adds a single node to the noisy graph object.
+        If the node already exists, nothing is performed.
+        :param node: hashable
+        """
+        if node not in self.nodes():
+            self.__real_edges[node] = set()
+            self.__fake_edges[node] = set()
+
+    def add_nodes_from(self, nodes):
+        """Adds multiple nodes from a node list.
+        If a node already exists in the graph, it is not
+        added again.
+        :param nodes: a list of nodes
+        """
+        for node in nodes:
+            self.add_node(node)
 
     @staticmethod
-    def number_of_hypotheses(no_total, no_false, exact=True):
+    def __get_edge(node1, node2):
+        """Returns a two-tuple with which elements are in increasing order.
+        :param node1: hashable
+        :param node2: hashable
+        :return: a two-tuple with which elements are in increasing order
+        """
+        return (node1, node2) if node1 < node2 else (node2, node1)
+
+    def edges_if(self, real=True):
+        """Returns a set of all edges that satisfy the `real`
+        condition.
+        :param real: boolean
+        :return: a set of two-tuples
+        """
+        graph_dictionary = self.__real_edges if real else self.__fake_edges
+        edge_set = set()
+
+        for node1, nodes in graph_dictionary.items():
+            for node2 in nodes:
+                edge = NoisyGraph.__get_edge(node1, node2)
+                edge_set.add(edge)
+
+        return edge_set
+
+    def edges(self):
+        """Return all edges in the graph, both real and fake.
+        :return: a set of two-tuples
+        """
+        real_edges = self.edges_if(real=True)
+        fake_edges = self.edges_if(real=False)
+        return real_edges.union(fake_edges)
+
+    def add_edge(self, node1, node2, real):
+        """
+        Adds a single edge to the graph. If the nodes in the edge
+        do not exists, they are added first to the graph. The `real`
+        parameter indicates whether the edge is real or fake. If the
+        edge already exists as the opposite (real or fake) it is updated.
+        :param node1: hashable object
+        :param node2: hashable object
+        :param real: boolean
+        """
+        if node1 not in self.nodes():
+            self.add_node(node1)
+        if node2 not in self.nodes():
+            self.add_node(node2)
+
+        if real:
+            self.__real_edges[node1].add(node2)
+            self.__fake_edges[node1].discard(node2)
+            self.__real_edges[node2].add(node1)
+            self.__fake_edges[node2].discard(node1)
+        else:
+            self.__fake_edges[node1].add(node2)
+            self.__real_edges[node1].discard(node2)
+            self.__fake_edges[node2].add(node1)
+            self.__real_edges[node2].discard(node1)
+
+    def add_edges_from(self, edge_list, real):
+        """Adds multiple edges to the graph. If the nodes in the edge
+        do not exists, they are added first to the graph. The `real`
+        parameter indicates whether the edge is real or fake. If the
+        edge already exists as the opposite (real or fake) it is updated.
+        :param edge_list: a list of tuples containing the edges as (node1, node2)
+        :param real: boolean
+        """
+        for node1, node2 in edge_list:
+            self.add_edge(node1, node2, real)
+
+    def node_adjacency_if(self, node, real=True):
+        """Returns a set of all edges connected to node that satisfy the
+        real condition.
+        :param node: hashable
+        :param real: boolean
+        :return: a set of tuples
+        """
+        adjacency_set = set()
+        if node in self.nodes():
+            graph_dictionary = self.__real_edges if real else self.__fake_edges
+            neighbors = graph_dictionary[node]
+            for neighbor in neighbors:
+                edge = NoisyGraph.__get_edge(node, neighbor)
+                adjacency_set.add(edge)
+
+        return adjacency_set
+
+    def node_adjacency(self, node):
+        """Returns a set of all edges connected to node.
+        :param node: hashable
+        :return: a set of tuples
+        """
+        real_edges = self.node_adjacency_if(node, True)
+        fake_edges = self.node_adjacency_if(node, False)
+        return real_edges.union(fake_edges)
+
+    def number_of_edges(self):
+        """Obtain the number of real, fake and total edges in the graph.
+        :return: 3-tuple
+        """
+        total = len(self.edges())
+        no_real_edges = len(self.edges_if(True))
+        no_fake_edges = total - no_real_edges
+        return no_real_edges, no_fake_edges, total
+
+    def number_of_edges_for_node(self, node):
+        """Obtain the number of real, fake and total
+        edges in the graph for node.
+        :param node: hashable
+        :return: 3-tuple (no_real_edges, no_fake_edges, total_edges)
+        """
+        if node not in self.nodes():
+            return None
+
+        total = len(self.node_adjacency(node))
+        no_real_edges = len(self.node_adjacency_if(True))
+        no_fake_edges = total - no_real_edges
+        return no_real_edges, no_fake_edges, total
+
+    @staticmethod
+    def __number_of_hypotheses(total_edges, fake_edges, exact=True):
+        """Calculates the number of hypotheses an attacker will need to
+        consider in case there are a total of 'fake_edges' among 'total_edges'
+        possible. The 'exact' parameter indicates whether the attacker knows
+        the exact number of fake edges (True) or the maximum number of
+        fake edges (False).
+        :param total_edges: integer
+        :param fake_edges: integer
+        :param exact: boolean
+        :return: integer
+        """
         no_hypotheses = 0
         if exact:
-            no_hypotheses = comb(no_total, no_false, exact=True)
+            no_hypotheses = comb(total_edges, fake_edges, exact=True)
         else:
-            for i in range(no_false + 1):
-                no_hypotheses += comb(no_total, i, exact=True)
+            for i in range(fake_edges + 1):
+                no_hypotheses += comb(total_edges, i, exact=True)
 
         return no_hypotheses
 
-    def get_nodes(self):
-        return [node for node in self.__graph.nodes]
-
-    def add_edge(self, n1, n2, real=True):
-        self.__graph.add_edge(n1, n2, real=real)
-
-    def add_edges_from(self, edge_list, real=True):
-        self.__graph.add_edges_from(edge_list, real=real)
-
-    def get_edges(self, real=None):
-        if real is None:
-            return [(n1, n2) for (n1, n2) in self.__graph.edges]
-
-        return [(n1, n2) for (n1, n2) in self.__graph.edges if self.__graph[n1][n2]['real'] == real]
-
-    def get_node_edges(self, node, real=None):
-        if node not in self.__graph.nodes:
-            return None
-
-        if real is None:
-            return [(node, n) for n in self.__graph[node]]
-
-        return [(node, n) for n in self.__graph[node] if self.__graph[node][n]['real'] == real]
-
-    def count_edges(self):
-        total = self.__graph.number_of_edges()
-        no_real_edges = len(self.get_edges(real=True))
-        no_false_edges = total - no_real_edges
-        return no_real_edges, no_false_edges, total
-
-    def count_node_edges(self, node):
-        if node not in self.__graph.nodes:
-            return None
-
-        total = len(self.get_node_edges(node))
-        no_real_edges = len(self.get_node_edges(node, real=True))
-        no_false_edges = total - no_real_edges
-        return no_real_edges, no_false_edges, total
-
     def uncertainty(self, base=2, exact=True):
-        _, no_false, no_total = self.count_edges()
-        no_hypotheses = NoisyGraph.number_of_hypotheses(no_total, no_false, exact)
+        """Calculates the graph uncertainty. The parameter `base` is used
+        to determine the units i.e., bits, trits, etc. If `exact` is set
+        to True it means that an attacker knows the exact number of fake
+        edges. If it is set to False it means the attacker knows the
+        maximum number of fake edges.
+        :param base: positive integer
+        :param exact: boolean
+        :return: integer
+        """
+        _, no_fake_edges, total_edges = self.number_of_edges()
+        no_hypotheses = NoisyGraph.__number_of_hypotheses(total_edges, no_fake_edges, exact)
         return log(no_hypotheses, base)
 
     def node_uncertainty(self, node, base=2, exact=True):
-        if node not in self.__graph.nodes:
+        """Calculates a given `node` uncertainty. The parameter `base` is
+        used to determine the units i.e., bits, trits, etc. If `exact` is
+        set to True it means that an attacker knows the exact number of fake
+        edges. If it is set to False it means the attacker knows the maximum
+        number of fake edges.
+        :param node: hashable
+        :param base: positive integer
+        :param exact: boolean
+        :return: integer or None if node does not exist in graph
+        """
+        if node not in self.nodes():
             return None
 
-        _, no_false, no_total = self.count_node_edges(node)
-        no_hypotheses = NoisyGraph.number_of_hypotheses(no_total, no_false, exact)
+        _, no_fake_edges, total_edges = self.number_of_edges_for_node(node)
+        no_hypotheses = NoisyGraph.__number_of_hypotheses(total_edges, no_fake_edges, exact)
         return log(no_hypotheses, base)
 
     def node_uncertainties(self, base=2, exact=True):
-        return [self.node_uncertainty(node, base, exact) for node in self.__graph.nodes]
+        """Calculates the uncertainty of all the nodes in the graph
+        and returns a list. The parameter `base` is  used to determine the units
+        i.e., bits, trits, etc. If `exact` is set to True it means that an attacker
+        knows the exact number of fake edges per node. If it is set to False it means
+        the attacker knows the maximum number of fake edges per node.
+        :param base: positive integer
+        :param exact: boolean
+        :return: list of floats
+        """
+        return [self.node_uncertainty(node, base, exact) for node in self.nodes()]
 
     def uncertainty_profile(self, base=2, exact=True):
+        """Calculates the mean and standard deviation of the
+        graph's nodes uncertainty. The parameter `base` is
+        used to determine the units i.e., bits, trits, etc.
+        If `exact` is set to True it means that an attacker knows
+        the exact number of fake edges per node. If it is set to False
+        it means the attacker knows the maximum number of fake edges
+        per node.
+        :param base: positive integer
+        :param exact: boolean
+        :return: tuple of floats corresponding to (mean, std_dev)
+        """
         uncertainties = self.node_uncertainties(base, exact)
         mean = sum(uncertainties) / len(uncertainties)
-        stdev = statistics.pstdev(uncertainties)
-        return mean, stdev
+        std_dev = statistics.pstdev(uncertainties)
+        return mean, std_dev
