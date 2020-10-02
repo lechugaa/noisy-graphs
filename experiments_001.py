4b7ff89f9ad34c25382aedf60275b7dbe2830cac
Antonio Lechuga <antoniolechugarivera@gmail.com>
2020-10-02 14:30:51 -0500

    Add experiment ideas

--- experiments_001.py
+++ experiments_001.py
@@ -1,6 +1,6 @@
 import networkx as nx
 from negative_graphs.noisy_graph import NoisyGraph
-from networkx.algorithms.centrality import group_betweenness_centrality
+from networkx.algorithms.centrality import group_betweenness_centrality, group_degree_centrality
 from networkx.algorithms.dominating import dominating_set
 
 
@@ -21,7 +21,7 @@ if __name__ == '__main__':
         noisy_ws.add_random_missing_edges(fraction)
         modified_ws = nx.Graph(noisy_ws.edges())
 
-        dom_group_centrality = group_betweenness_centrality(modified_ws, ws_dom_set)
+        dom_group_centrality = group_degree_centrality(modified_ws, ws_dom_set)
         graph_uncertainty = noisy_ws.uncertainty()
         mean_uncertainty, std_dev_uncertainty = noisy_ws.uncertainty_profile()
         print(f'{fraction}, {dom_group_centrality}, {graph_uncertainty}, {mean_uncertainty}, {std_dev_uncertainty}')
