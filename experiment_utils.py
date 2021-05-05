dc008cbc7f4e167e9cce72ba9866365bea88ae28
lechugaa <>
2021-05-04 23:41:26 -0500

    minor fix

--- experiment_utils.py
+++ experiment_utils.py
@@ -3,9 +3,9 @@ from noisy_graphs.noisy_graph import NoisyGraph
 
 
 def create_data_path_file(data_path: str):
-    header = "exp_name, sigma_mean, sigma_variance, uncertainty_mean, uncertainty_variance, dc_distance, " \
-             "dc_correlation, dc_mean_change, bc_distance, bc_correlation, bc_mean_change, cc_distance, " \
-             "cc_correlation, cc_mean_change, ec_distance, ec_correlation, ec_mean_change\n"
+    header = "exp_name,sigma_mean,sigma_variance,uncertainty_mean,uncertainty_variance,dc_distance," \
+             "dc_correlation,dc_mean_change,bc_distance,bc_correlation,bc_mean_change,cc_distance," \
+             "cc_correlation,cc_mean_change,ec_distance,ec_correlation,ec_mean_change\n"
     f = open(data_path, "w")
     f.write(header)
     f.close()
@@ -42,11 +42,11 @@ def perform_experiment(original_graph: nx.Graph, ftrp: float, exp_name: str, dat
     f.close()
 
     # add result to csv
-    result = f"{exp_name}, {sigma_mean}, {sigma_variance}, {uncertainty_mean}, {uncertainty_variance}, "
-    result += f"{dc_distance}, {dc_correlation}, {dc_mean_change}, "
-    result += f"{bc_distance}, {bc_correlation}, {bc_mean_change}, "
-    result += f"{cc_distance}, {cc_correlation}, {cc_mean_change}, "
-    result += f"{ec_distance}, {ec_correlation}, {ec_mean_change}"
+    result = f"{exp_name},{sigma_mean},{sigma_variance},{uncertainty_mean},{uncertainty_variance},"
+    result += f"{dc_distance},{dc_correlation},{dc_mean_change},"
+    result += f"{bc_distance},{bc_correlation},{bc_mean_change},"
+    result += f"{cc_distance},{cc_correlation},{cc_mean_change},"
+    result += f"{ec_distance},{ec_correlation},{ec_mean_change}"
     result += "\n"
 
     f = open(data_path, "a")
