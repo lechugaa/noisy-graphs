1aae8f51f3cf041bc00b0e6f684252864898e060
Antonio Lechuga <antoniolechugarivera@gmail.com>
2021-10-06 08:45:32 -0500

    change results grid

--- debug_experiments.py
+++ debug_experiments.py
@@ -3,7 +3,7 @@ from noisy_graphs.noisy_graph import NoisyGraph
 
 
 if __name__ == '__main__':
-    graph = nx.erdos_renyi_graph(n=200, p=0.7)
+    graph = nx.barabasi_albert_graph(n=20, m=3)
     n_graph = NoisyGraph(ftrp=0.5)
     n_graph.construct_graph(graph)
     print(n_graph.degree_centrality_profile(graph))
